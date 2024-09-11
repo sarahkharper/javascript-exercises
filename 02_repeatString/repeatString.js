6c60521c70165ef406262c1aac23480a72cd930c
sarahkharper <skhaao@gmail.com>
2024-09-11 16:22:51 -0700

    Solution to exercise 02 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+const repeatString = function(strng, reps) {
+    if(reps < 0) return "ERROR"
+    let stringHolder = "";
+    for(let i = 0; i < reps; i++){
+        stringHolder = stringHolder.concat(strng);
+        }
+    return stringHolder;
+ };
 
 // Do not edit below this line
 module.exports = repeatString;
