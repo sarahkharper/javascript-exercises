d7eda03979d8654e753f3206d11c05132f2c42e5
sarahkharper <skhaao@gmail.com>
2024-09-12 12:12:35 -0700

    Solution to exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    //split any separate words in the string into separate array items
+    if(inputString === ""){
+        return inputString;
+    }
+    let splitArray = inputString.split("");
+    let reversedArray = [];
+    for(i = 0; i < splitArray.length; i++){
+        reversedArray.unshift(splitArray[i]);
+    }
+    let reversedString = reversedArray.join("");
+    return reversedString;
 };
+
+//SIMPLER SOLUTION: return string.split("").reverse().join("");
 
 // Do not edit below this line
 module.exports = reverseString;
