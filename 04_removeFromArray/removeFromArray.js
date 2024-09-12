5f17718348a95625325b659866470511ee7b22e1
sarahkharper <skhaao@gmail.com>
2024-09-12 15:11:09 -0700

    Solution for exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(baseArray, ...itemsToRemove) {
+    //create empty array to hold indexes of items to remove
+    let indexes = [];
+
+    //set up for loop to iterate through the items to remove
+    for(const item of itemsToRemove){
+        //check if item is in base array, skip if not
+        if (baseArray.indexOf(item) > -1){
+            //check if item is at each index in base array
+            for(i = 0; i < baseArray.length; i++){ 
+                if(baseArray[i] === item){
+                    indexes.push(i);
+                } 
+            }
+        }
+    }
+    //sort array of indexes in descending order
+    //console.log(indexes);
+    indexes.sort().reverse();
+    //remove items at designated indexes from array
+    for(j = 0; j < indexes.length; j++){
+        baseArray.splice(indexes[j],1);
+    }
+
+    return baseArray;
 };
 
 // Do not edit below this line
