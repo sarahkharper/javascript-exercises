36b8d9b3d0d7bf1a8432958b25d9372c06f874f9
sarahkharper <skhaao@gmail.com>
2024-09-13 11:18:31 -0700

    Solution to exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputTemp) {
+  //convert input in fahrenheit to celsius
+  if(typeof inputTemp === "number"){
+    let celsiusTemp = (inputTemp - 32) * (5/9);
+    //if not an integer, round to 1 decimal 
+    if(!Number.isInteger(celsiusTemp)){
+      celsiusTemp = celsiusTemp.toFixed(1)*1;
+    }
+    return celsiusTemp;
+  } else {alert("You must input a number!")}
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputTemp) {
+  if(typeof inputTemp === "number"){
+    let fahrenheitTemp = inputTemp * (9/5) + 32;
+    //if not an integer, round to 1 decimal 
+    if(!Number.isInteger(fahrenheitTemp)){
+      fahrenheitTemp = fahrenheitTemp.toFixed(1)*1;
+    }
+    return fahrenheitTemp;
+  } else {alert("You must input a number!")}
 };
 
 // Do not edit below this line
