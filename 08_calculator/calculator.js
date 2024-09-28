a95d6bbe890bc4d61def5c3001dbe0741d34bfc8
sarahkharper <skhaao@gmail.com>
2024-09-27 19:00:59 -0700

    Solution to exercise 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	if (typeof a == "number" && typeof b == "number"){
+    return a + b;
+  }
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  if (typeof a == "number" && typeof b == "number"){
+    return a - b;
+  }
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.every(item => typeof item === 'number')){
+    return arr.reduce((total, item) => total + item, 0);
+  } else {
+    return 0;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr.every(item => typeof item === 'number')){
+    return arr.reduce((total, item) => total * item, 1);
+  } else {
+    return undefined;
+  };
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(n === 0) return 1;
+  let result = 1;
+  for(n = 2; n <= a; n++){
+    result *= n;
+  }
+  return result;
 };
 
 // Do not edit below this line
